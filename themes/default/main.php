cf8e56b46ce8eff94638b42d5bfd8c88290a22b1
ankit5 <vermankit5@gmail.com>
2024-04-30 15:27:31 +0530

    dd

--- themes/default/main.php
+++ themes/default/main.php
@@ -1,5 +1,5 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-
+<meta name="viewport" content="width=device-width,initial-scale=1">
 <html>
 <head>
 <title></title>
@@ -13,7 +13,7 @@ $url = str_replace('/?url=','',"$_SERVER[REQUEST_URI]");
 ?>
 		<form action="includes/process.php?action=update" method="post"  class="form">
 			<input type="hidden" name="u" value="<?php print $url?>" id="input" size="40" class="textbox">
-			<input type="submit" value="Download" class="button"> 
+			<input type="submit" value="Download" class="button" style="width:100%;"> 
 			
 			<br style="clear: both;">
 		</form>
